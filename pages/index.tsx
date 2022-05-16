d7db891c3017a0c58b588b811b424572a7799b86
Bryce Hebert <brycehebert@users.noreply.github.com>
2022-05-16 06:21:13 -0500

    Use a table instead of CSS grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
-import React from "react";
 import dynamic from "next/dynamic";
 import { GetServerSideProps } from "next";
-import axios from "axios";
+import { prisma } from "../server/prisma";
+import Plane from "../components/Plane";
+import styles from "../styles/Home.module.css";
 
-const index = (): React.ReactElement => {
+const index = ({ data }: any): JSX.Element => {
   // React-Leaflet doesn't work with SSR, must do Client-Side
   const MapWithNoSSR = dynamic(() => import("../components/Map"), { ssr: false });
+
   return (
-    <div >
-      <MapWithNoSSR />
+    <div className={styles.mainContainer}>
+      <table className={styles.flightTable}>
+        <caption>Most Recent Sightings</caption>
+        <thead>
+          <tr>
+            <th></th>
+            <th scope="col">Call Sign</th>
+            <th scope="col">First Seen</th>
+            <th scope="col">Last Seen</th>
+            <th scope="col">ICAO Hex</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((el: any) => (
+            <Plane {...el} key={el.id} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
@@ -16,11 +34,24 @@ const index = (): React.ReactElement => {
 export default index;
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  let data = await prisma.airplane.findMany({
+    take: 15,
+    orderBy: { lastSeen: "desc" },
+    include: {
+      Messages: { take: 5, select: { callSign: true } }
+    }
+  });
 
-  const res = await axios.get
+  //@ts-expect-error
+  data = data.map((el) => {
+    return {
+      ...el,
+      createdAt: el.createdAt.toLocaleString(),
+      lastSeen: el.lastSeen.toLocaleString()
+    };
+  });
 
   return {
-    props: {}
-  }
-
-}
+    props: { data }
+  };
+};
