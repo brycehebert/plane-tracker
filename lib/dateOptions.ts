6c821d7b0756534dc4a47beb236b0154cc4958ee
Bryce Hebert <brycehebert@users.noreply.github.com>
2022-05-17 07:01:34 -0500

    Add dateOptions

---
+++ lib/dateOptions.ts
@@ -0,0 +1,10 @@
+const dateOptions: Intl.DateTimeFormatOptions = {
+  day: "2-digit",
+  month: "2-digit",
+  year: "2-digit",
+  hour12: true,
+  hour: "numeric",
+  minute: "2-digit"
+};
+
+export default dateOptions;
