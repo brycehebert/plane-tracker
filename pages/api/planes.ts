71cb0dca05b47d535176b794c144ea4a71c4e3dc
Bryce Hebert <brycehebert@users.noreply.github.com>
2022-05-19 07:19:10 -0500

    Making Prisma grab data

--- pages/api/planes.ts
+++ pages/api/planes.ts
@@ -1,14 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/prisma";
 
-const handler = async (req: NextApiRequest, res:NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-  let data = await prisma.airplane.findMany();
-  res.status(200).json(data);
-  } catch (e) {
-    
-  }
-
-}
+    let data = await prisma.airplane.findMany({
+      take: 10,
+      orderBy: { lastSeen: "desc" },
+      include: {
+        Messages: { select: { callSign: true, lat: true, lon: true, alt: true, flightId: true } },
+        Flights: true
+      }
+    });
+    data;
+    res.status(200).json(data);
+  } catch (e) {}
+};
 
 export default handler;
