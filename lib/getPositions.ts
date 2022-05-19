c4b66c6ba7fac53b8f991313a2d6a7c70e35812b
Bryce Hebert <brycehebert@users.noreply.github.com>
2022-05-19 07:16:48 -0500

    Add getPositions utility

---
+++ lib/getPositions.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const getPositions = async (flightId: string) => {
+  try {
+    let result = await axios.get(`/api/flights/${flightId}`);
+    const messages = result.data.Messages;
+    let positions: number[][] = [];
+
+    for (let el of messages) {
+      positions.push([el.lat, el.lon]);
+    }
+    
+    return positions;
+
+  } catch (err) {
+    console.log(err);
+    return [[]];
+  }
+};
+
+export default getPositions;
